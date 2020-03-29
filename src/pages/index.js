f0da2d05cbc16783322684da7a3efaa61022f5b6
Colby Fayock <fayock@gmail.com>
2020-03-29 12:21:54 -0400

    Step 3: creating our geographic data

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,27 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if ( !hasData ) return;
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        }
+      })
+    }
   }
 
   const mapSettings = {
