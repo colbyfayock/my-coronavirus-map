86bebfee4a34b9bad516879b228921cdaad55126
Colby Fayock <fayock@gmail.com>
2020-03-29 12:10:38 -0400

    Step 2: adding axios and fetching the coronavirus data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
+import axios from 'axios';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
@@ -22,6 +23,16 @@ const IndexPage = () => {
    */
 
   async function mapEffect({ leafletElement: map } = {}) {
+    let response;
+
+    try {
+      response = await axios.get('https://corona.lmao.ninja/countries');
+    } catch(e) {
+      console.log(`Failed to fetch countries: ${e.message}`, e);
+      return;
+    }
+
+    const { data = [] } = response;
   }
 
   const mapSettings = {
