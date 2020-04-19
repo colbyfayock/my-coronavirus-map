fe9d85e57f7474a86d38213676bf62df4b6168a4
Colby Fayock <fayock@gmail.com>
2020-04-19 12:27:18 -0400

    Step 1: create a hook to fetch the data, fetch data outside of effect, use hook to fetch both country data and overall stats

---
+++ src/hooks/useTracker.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_HOST = 'https://corona.lmao.ninja/v2';
+
+const ENDPOINTS = [
+  {
+    id: 'all',
+    path: '/all',
+    isDefault: true
+  },
+  {
+    id: 'countries',
+    path: '/countries'
+  }
+]
+
+const defaultState = {
+  data: null,
+  state: 'ready'
+}
+
+const useTracker = ({ api = 'all' }) => {
+
+  const [tracker = {}, updateTracker] = useState(defaultState)
+
+  async function fetchTracker() {
+    let route = ENDPOINTS.find(({ id } = {}) => id === api);
+
+    if ( !route ) {
+      route = ENDPOINTS.find(({ isDefault } = {}) => !!isDefault);
+    }
+
+    let response;
+
+    try {
+      updateTracker((prev) => {
+        return {
+          ...prev,
+          state: 'loading'
+        }
+      });
+      response = await axios.get(`${API_HOST}${route.path}`);
+    } catch(e) {
+      updateTracker((prev) => {
+        return {
+          ...prev,
+          state: 'error',
+          error: e
+        }
+      });
+      return;
+    }
+
+    const { data } = response;
+
+    updateTracker((prev) => {
+      return {
+        ...prev,
+        state: 'ready',
+        data
+      }
+    });
+
+  }
+
+  useEffect(() => {
+    fetchTracker()
+  }, [api])
+
+  return {
+    fetchTracker,
+    ...tracker
+  }
+};
+
+export default useTracker;
